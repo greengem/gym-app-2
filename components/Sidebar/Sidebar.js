906ca0c68574c2b1784993316c2fae5dddd881f7
Green Gem <chris_waitt@me.com>
2023-10-16 11:44:25 +0100

    usePathname to show which page is active

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -4,7 +4,6 @@ export default function Sidebar() {
     return (
         <>
             <div className='p-5'>
-                <h2 className='text-2xl mb-5'>Sidebar</h2>
                 <SidebarNav />
             </div>
         </>
--- components/Sidebar/SidebarNav.js
+++ components/Sidebar/SidebarNav.js
@@ -1,10 +1,15 @@
+"use client";
+import { usePathname } from 'next/navigation'
 import Link from "next/link";
 
 export default function SidebarNav() {
+    const pathname = usePathname()
     return (
         <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/dashboard">Dashboard</Link></li>
+            <li className={pathname === "/" ? "text-blue-500" : ""}><Link href="/">Home</Link></li>
+            <li className={pathname === "/dashboard" ? "text-blue-500" : ""}><Link href="/dashboard">Dashboard</Link></li>
+            <li className={pathname === "/workout" ? "text-blue-500" : ""}><Link href="/workout">Start Workout</Link></li>
+            <li className={pathname === "/routines" ? "text-blue-500" : ""}><Link href="/routines">Routines</Link></li>
         </ul>
     )
 }
