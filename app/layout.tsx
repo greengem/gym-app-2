b816e47f8753bb5287c8eaf94db41cf54ec84c33
Green Gem <chris_waitt@me.com>
2023-10-16 19:02:54 +0100

    Installed react-hot-toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Providers } from '@/app/providers'
+import { Toaster } from 'react-hot-toast';
 import Sidebar from '@/components/Sidebar/Sidebar';
 
 export default function RootLayout({
@@ -11,6 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
+          <Toaster />
           <main className="flex">
             <div className="fixed top-0 left-0 h-full w-64 bg-gray-800 text-white"><Sidebar /></div>
             <div className="ml-64 p-5">{children}</div>
