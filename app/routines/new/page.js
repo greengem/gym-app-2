30be36ab2ce2508b0e1be2aa18f50ad5d5d78e43
Green Gem <chris_waitt@me.com>
2023-10-16 13:27:04 +0100

    Removed flowbite ui library

--- app/routines/new/page.js
+++ app/routines/new/page.js
@@ -5,7 +5,40 @@ export default async function RoutinesPage() {
   return (
     <>
     <PageHeading pageTitle="Create New Routine" />
-    <Link href="/routines">Back</Link>
+    <div>
+      <input 
+        type='text' 
+        name='routineName'
+        placeholder='Routine Name'
+      />
+      <input 
+        type='text' 
+        name='routineNotes'
+        placeholder='Notes'
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Exercise</th>
+            <th>Sets</th>
+            <th>Reps</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bench Press</td>
+            <td>8</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>Deadlift</td>
+            <td>8</td>
+            <td>3</td>
+          </tr>
+        </tbody>
+      </table>
+      <button>Submit</button>
+    </div>
 
     </>
   )
--- app/routines/page.js
+++ app/routines/page.js
@@ -1,5 +1,6 @@
 import prisma from '@/db/prisma';
 import PageHeading from '@/components/PageHeading/PageHeading'
+import Link from 'next/link';
 
 async function getRoutines(){
   const routines = await prisma.workoutPlan.findMany();
@@ -12,6 +13,7 @@ export default async function RoutinesPage() {
   return (
     <>
       <PageHeading pageTitle="Routines" />
+      <Link href="/routines/new">New Routine</Link>
       <ul>
         {routines.map((routine) => (
           <li key={routine.id}>{routine.name}</li>
