5792d7c3b18d3dc7408e49d2d62366c17e6deca1
Green Gem <chris_waitt@me.com>
2023-10-16 17:32:36 +0100

    I can login and logout now, the config needs some refinement and im having trouble returning the username even though it is set in the session

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,23 +1,35 @@
+import { auth } from "@/auth/lucia";
+import * as context from "next/headers";
+import { redirect } from "next/navigation";
 import prisma from '@/db/prisma';
+
 import PageHeading from '@/components/PageHeading/PageHeading'
+import Form from "@/components/form";
 
 async function getWorkouts(){
-  const workouts = await prisma.workoutLog.findMany();
-  return workouts;
+	const workouts = await prisma.workoutLog.findMany();
+	return workouts;
+  }
+
+export default async function DashboardPage() {
+	const authRequest = auth.handleRequest("GET", context);
+	const session = await authRequest.validate();
+	const workouts = await getWorkouts()
+
+	if (!session) redirect("/login");
+	return (
+		<>
+			<PageHeading pageTitle="Dashboard" />
+			<p>User id: {session.user.userId}</p>
+			<p>Username: {session.user.username}</p>
+			<Form action="/api/logout">
+				<input type="submit" value="Sign out" />
+			</Form>
+			<ul>
+				{workouts.map((workout) => (
+				<li key={workout.id}>{workout.name}</li>
+				))}
+			</ul>
+		</>
+	);
 }
-
-export default async function WorkoutsPage() {
-  const workouts = await getWorkouts()
-
-  return (
-    <>
-      <PageHeading pageTitle="Dashboard" />
-      <ul>
-        {workouts.map((workout) => (
-          <li key={workout.id}>{workout.name}</li>
-        ))}
-      </ul>
-    </>
-  )
-}
-
